5565210e93f10e489a473a1d7498866e6864cc05
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-01-12 11:23:11 -0800

    Add dark theme and light theme buttons

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,18 @@ $(document).ready(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
   });
+
   $(".clickable").click(function() {
     $("#show-image").toggle();
     $("#hide-image").toggle();
+  });
+
+  $("button#darkmode").click(function() {
+    $("body").removeClass();
+    $("body").addClass("darktheme");
+  });
+  $("button#lightmode").click(function() {
+    $("body").removeClass();
+    $("body").addClass("lighttheme");
   });
 });
